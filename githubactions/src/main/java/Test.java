cb0333ecb80b4bb386e5189169bb7bfb197584e3
nitinkumarn <nitinkumarsuccess@gmail.com>
2025-03-22 15:39:46 +0530

    Added sample java class

---
+++ githubactions/src/main/java/Test.java
@@ -0,0 +1,12 @@
+import java.time.LocalTime;
+
+class Test{
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+
+        // Print the current time
+        System.out.println("Current time: " + LocalTime.now());
+        
+    }
+}
